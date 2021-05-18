145cc596d0cc1185d20bb56d0d819833681b85bb
issuh <yoyus5@naver.com>
2021-05-19 01:01:48 +0900

    update go server

--- cmd/remote_display_server/main.go
+++ cmd/remote_display_server/main.go
@@ -13,6 +13,14 @@ type ScreenCapture struct {
 	handle unsafe.Pointer
 }
 
+type ScreemInfo struct {
+	id     int
+	width  int
+	height int
+	x      int
+	y      int
+}
+
 func CreateScreenCaptureHandle() ScreenCapture {
 	var screen_capture ScreenCapture
 	screen_capture.handle = C.create_rdv_hadle()
@@ -27,8 +35,22 @@ func ScreenCount(screen_capture *ScreenCapture) int {
 	return int(C.screen_count(screen_capture.handle))
 }
 
-func ScreenInfomations(screen_capture *ScreenCapture, screen_info ) {
+func ScreenInfomations(screen_capture *ScreenCapture) []ScreemInfo {
+	count := ScreenCount(screen_capture)
+	infomations := make([]ScreemInfo, count, count)
+	screen_info := make([]C.RemoteScreen, count, count)
 
+	C.screen_infomations(screen_capture.handle, &screen_info[0])
+
+	for i, info := range screen_info {
+		infomations[i].id = int(info.id)
+		infomations[i].width = int(info.width)
+		infomations[i].height = int(info.height)
+		infomations[i].x = int(info.x)
+		infomations[i].y = int(info.y)
+	}
+
+	return infomations
 }
 
 func main() {
@@ -36,9 +58,14 @@ func main() {
 
 	fmt.Println("Screen Capture Handle : ", testHandle.handle)
 
-	fmt.Println("Screen Count : ", ScreenCount(&testHandle))
+	count := ScreenCount(&testHandle)
+	fmt.Println("Screen Count : ", count)
 
-	var remote_screen_info []C.RemoteScreen
+	screen_info := ScreenInfomations(&testHandle)
+
+	for _, info := range screen_info {
+		fmt.Println("[", info.id, "] : ", info.width, "x", info.height, "/", info.x, ",", info.y)
+	}
 
 	DestroyScreenCaptureHandle(&testHandle)
 }
